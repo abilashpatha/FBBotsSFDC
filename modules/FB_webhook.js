175f5ccf57ae35a9ae196a3a427359c7176ddfb1
rajdadeveloper <33024957+rajdadeveloper@users.noreply.github.com>
2017-10-25 15:18:32 +0530

    commit
    commit

--- modules/FB_webhook.js
+++ modules/FB_webhook.js
@@ -61,7 +61,7 @@ exports.webhookPost = function(req,res)
 		var sender = event.sender.id;
 		if(event.message && event.message.text){
 			console.log("FACEBOOK ID IS:" +sender);
-			console.log("all values in event: " +event.recipient.id);
+			console.log("all values in event: " +event.recipient.phone_number);
 			//botResponse({text:'Hello I am AWESOME BOT to help you'}, sender);
 			sInterpret({text:event.message.text}, sender);
 		}
